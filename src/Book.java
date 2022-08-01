5dba87d101dc69705ca2f58a7b8f8df6a43189b6
gopalamjava <gopalamsai14@gmail.com>
2022-08-01 13:34:48 -0400

    First commit

---
+++ src/Book.java
@@ -0,0 +1,56 @@
+
+public class Book {		
+        private String title;
+		private String author;
+		private int publicationYear;
+		private double averageRating;
+		private int ratingsCount;
+		private String imageUrl;
+		
+		// Add getters & setters for author, averageRating, and ratingsCount
+		
+		public String getTitle() {
+			return title;
+		}
+		public void setTitle(String title) {
+			this.title = title;
+		}
+		
+		public int getPublicationYear() {
+			return publicationYear;
+		}
+		public void setPublicationYear(int publicationYear) {
+			this.publicationYear = publicationYear;
+		}
+		
+		public String getImageUrl() {
+			return imageUrl;
+		}
+		public void setImageUrl(String imageUrl) {
+			this.imageUrl = imageUrl;
+		}
+		public String getAuthor() {
+			return author;
+		}
+		public void setAuthor(String author) {
+			this.author = author;
+		}
+		public double getAverageRating() {
+			return averageRating;
+		}
+		@Override
+		public String toString() {
+			return "Book [title=" + title + ", author=" + author + ", publicationYear=" + publicationYear
+					+ ", averageRating=" + averageRating + ", ratingsCount=" + ratingsCount + ", imageUrl=" + imageUrl
+					+ "]";
+		}
+		public void setAverageRating(double averageRating) {
+			this.averageRating = averageRating;
+		}
+		public int getRatingsCount() {
+			return ratingsCount;
+		}
+		public void setRatingsCount(int ratingsCount) {
+			this.ratingsCount = ratingsCount;
+		}
+	}
